2ca191e19b8811baadbc1454ee5b917e0d1aed3f
KARAN PRAVEEN <karansiamintern@gmail.com>
2021-04-19 15:04:40 +0530

    Form completeion/ To retain values after submit

---
+++ form_completion.php
@@ -0,0 +1,23 @@
+/* This is the part of the form of the php file where we 
+   can retain the values entered in the form till the form is submitted */
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">  //Here we are keeping the values entered even after submission
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">   // The echo function is used to carry it out
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website" value="<?php echo $website;?>">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <br><br>
+  Gender:  // Gender is a radio type input so we need to check it and cant use echo. This is done with the if and echo functions
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
